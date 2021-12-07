483674c5da6e0628c8e9a3cb32a36b35eadb5864
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-07 12:31:43 -0800

    added flag images to countrycard

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,10 +1,11 @@
 import '../styles/CountryCard.css';
 
-export default function CountryCard({ name, continent }) {
+export default function CountryCard({ name, iso2 }) {
   return (
     <div>
       <div>{name}</div>
-      <div>{continent}</div>
+      {/* <div>{continent}</div> */}
+      <img src={`https://flagcdn.com/84x63/${iso2.toLowerCase()}.png`} />
     </div>
   );
 }
