6697668425ad85954db7b9b69d82f5568897bbe2
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-06 17:22:23 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+// import Main from './components/Main/Main';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
-      <Footer />
+      {/* <Main />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+import '../styles/Header.css';
+
+export default function Header() {
+  return <header>Countries Of The World</header>;
+}
