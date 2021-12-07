32eec9db713a762061e16064333f6da11057f1ce
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-07 12:34:15 -0800

    added styling to countrycard

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -2,8 +2,8 @@ import '../styles/CountryCard.css';
 
 export default function CountryCard({ name, iso2 }) {
   return (
-    <div>
-      <div>{name}</div>
+    <div className="card">
+      <div className="countryName">{name}</div>
       {/* <div>{continent}</div> */}
       <img src={`https://flagcdn.com/84x63/${iso2.toLowerCase()}.png`} />
     </div>
